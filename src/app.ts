1af5c2a07071192cd3bea3df809fd425c3685c01
Mauro <m.henriquesilva18@gmail.com>
2023-05-07 21:50:31 +0200

    feat: add express-session options & cookieparser

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,33 @@
 import express from 'express'
 import cors from 'cors'
 import { routesV1 } from './routes/v1'
+import cookieParser from 'cookie-parser'
+import { randomString } from './utils/randomString'
+import session from 'express-session'
 
 const application = async () => {
     const app = express()
     app.use(express.json())
+    app.use(cookieParser())
 
     app.use(
         cors({
             origin: 'http://localhost:3000',
             credentials: true,
+        }),
+    )
+    const sessionSecret: string = randomString()
+
+    app.use(
+        session({
+            secret: sessionSecret,
+            resave: false,
+            saveUninitialized: true,
+            unset: 'destroy',
+            name: '_session',
+            genid: (req) => {
+                return randomString()
+            },
         }),
     )
 
