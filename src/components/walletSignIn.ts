afc996cbca91eaf7c3bc1196ec5c094b4a034277
Mauro <m.henriquesilva18@gmail.com>
2023-05-14 22:24:39 +0200

    fix: change wallet signature to frontend

--- src/components/walletSignIn.ts
+++ src/components/walletSignIn.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { Request, Response, NextFunction } from 'express'
 import { ethers, verifyMessage } from 'ethers'
+import session from './../@types/express-session/index'
 
 // API KEYS:
 const apiKey: string = process.env.ALCHEMY_API_KEY as string
@@ -14,15 +15,10 @@ const signer = new ethers.Wallet(privateKey, alchemyProvider)
 export const walletSignIn = async (req: Request, res: Response, next: NextFunction) => {
     try {
         // receiving user's data
-        const { address, chainId, signature } = req.body
-
-        // test
-        const secretCode = process.env.SIGNATURE_KEY as string
-        const secretSignature = `Message for address: ${address} and chain ${chainId} with code: ${secretCode}`
-        //
+        const { address, chainId, signature, message } = req.body
 
         // Verify signature with Message
-        const recoveredAddress = verifyMessage(secretSignature as string, signature)
+        const recoveredAddress = verifyMessage(message as string, signature)
         const isValidSignature = recoveredAddress === address
 
         // Validate user signature
@@ -30,27 +26,14 @@ export const walletSignIn = async (req: Request, res: Response, next: NextFuncti
             throw new Error('Invalid signature')
         }
 
-        res.cookie(
-            'user',
-            {
-                address: address,
-                chainId: chainId,
-                signature: signature,
-            },
-            {
-                httpOnly: true,
-                secure: false, // set to true in production
-                sameSite: 'strict',
-                maxAge: 1000 * 60 * 60 * 24,
-            },
-        )
         // TODO:  --## Check session types ##--
-        // // Store user data in the session
-        // req.session.user = {
-        //     address: address,
-        //     chainId: chainId,
-        //     signature: signature,
-        // }
+        // Store user data in the session
+
+        req.session.user = {
+            address: address,
+            chainId: chainId,
+            signature: signature,
+        }
 
         res.status(200).send('Successfully authenticated')
     } catch (error) {
